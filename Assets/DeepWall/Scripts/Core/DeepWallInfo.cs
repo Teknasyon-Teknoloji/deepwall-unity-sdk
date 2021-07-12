b9cae70d4ab733228eb6b6097c2509c1e3417fe3
İbrahim Kasapoğlu <ikasapogluu@gmail.com>
2021-07-12 04:14:05 +0300

    [release] Deepwall-Unity v1.2.0

--- Assets/DeepWall/Scripts/Core/DeepWallInfo.cs
+++ Assets/DeepWall/Scripts/Core/DeepWallInfo.cs
@@ -2,8 +2,8 @@ namespace DeepWallModule
 {
     public struct DeepWallInfo
     {
-        public const string DeepWallVersionName = "1.1.0";
-        public const string DeepWallVersionCode = "2";
+        public const string DeepWallVersionName = "1.2.0";
+        public const string DeepWallVersionCode = "3";
         
         public const string DeepWallDocUrl = "https://github.com/Teknasyon-Teknoloji/deepwall-unity-sdk/blob/main/README.md";
         public const string DeepWallDashboardUrl = "https://console.deepwall.com/";
